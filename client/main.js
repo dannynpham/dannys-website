8b55a857b4222c526e44311ae5fadc57ac02f8ea
Danny Pham <email": null>
2016-08-08 15:37:44 -0700

    started app over

--- client/main.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-import angularMeteor from 'angular-meteor';
-import websiteHeader from '../imports/components/websiteHeader/websiteHeader';
-
-angular.module('dannysWebsite', [
-  angularMeteor,
-  websiteHeader.name
-]);
